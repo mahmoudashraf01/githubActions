51edf9d0fb331f42d3aa87341f72ca8924cdf361
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2026-02-07 01:59:40 +0200

    modify test file

--- src/tests.sh
+++ src/tests.sh
@@ -3,9 +3,14 @@ EXPECTED="Hello, Test!"
 OUTPUT=$(node -e "console.log(require('./srs/app')('Test!'));")
 
 if [ "$OUTPUT" = "$EXPECTED" ]; then
-  echo "Test passed!"
-  exit 0
+
+echo "Test passed!"
+
+exit 0
+
 else
-  echo "Test failed: expected '$EXPECTED' but got '$OUTPUT'"
-  exit 1
+
+echo "Test failed: expected '$EXPECTED' but got '$OUTPUT'"
+
+exit 1
 fi
